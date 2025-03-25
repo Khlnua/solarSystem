f9f9d5253089a5f90bb4fe94e84e43710ace4198
25LP9675 25LP9675 <25LP9675@Admins-iMac.local>
2025-03-25 12:36:40 +0800

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+const Planet = () => {
+  return (
+    <div style={{ position: "relative" }}>
+      <img src="/images/galaxy.webp" alt="" className="mainPicture" />
+    </div>
+  );
+};
+export default Planet;
